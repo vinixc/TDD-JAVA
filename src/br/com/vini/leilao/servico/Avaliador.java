5ff6f45ec114c04e0451b7fd4a04ef089341f9f2
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-05-16 12:21:39 -0300

    criando projeto e primeiro teste

---
+++ src/br/com/vini/leilao/servico/Avaliador.java
@@ -0,0 +1,26 @@
+package br.com.vini.leilao.servico;
+
+import br.com.vini.leilao.dominio.Lance;
+import br.com.vini.leilao.dominio.Leilao;
+
+public class Avaliador {
+	
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
+
+	public void avalia(Leilao leilao) {
+		for(Lance lance : leilao.getLances()) {
+			if(lance.getValor() > maiorDeTodos ) maiorDeTodos = lance.getValor();
+			if(lance.getValor() < menorDeTodos ) menorDeTodos = lance.getValor();
+		}
+	}
+
+	public double getMaiorLance() {
+		return maiorDeTodos;
+	}
+	
+	public double getMenorLance() {
+		return menorDeTodos;
+	}
+
+}
---
+++ src/br/com/vini/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,37 @@
+package br.com.vini.leilao.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.vini.leilao.dominio.Lance;
+import br.com.vini.leilao.dominio.Leilao;
+import br.com.vini.leilao.dominio.Usuario;
+import br.com.vini.leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+	
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {
+		Usuario joao = new Usuario("joao");
+		Usuario jose = new Usuario("jose");
+		Usuario maria = new Usuario("maria");
+		
+		Leilao leilao = new Leilao("Play 3 Novo");
+		
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		
+		leiloeiro.avalia(leilao);
+		
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		
+	}
+
+}
